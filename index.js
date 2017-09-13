3e56e96458b5883466e18026b0135af56de57b9e
Berislav Grgičak <berislav.grgicak@gmail.com>
2017-09-13 11:48:40 +0200

    create log folder if not existing

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const fs = require('fs'),
       logExtension = '.log',
       apiKeys = ['n6b7rbi6x21ez3dlkp']//Temp api key db
 
+//Create log folder if doesn't exist
+if (!fs.existsSync(logFolder)){
+  fs.mkdirSync(logFolder)
+}
 
 //Temp api key validation
 function isValidApiKey(apiKey){
@@ -17,9 +21,12 @@ function isValidApiKey(apiKey){
   }
   return false
 }
+//Generata file path based on file name
 function filePath(fileName){
   return logFolder + fileName + logExtension
 }
+
+//Get log content
 app.get('/:log/:key', function(req, res){
   if(isValidApiKey(req.params.key)){
     res.set('Content-Type', 'text/plain')
@@ -39,6 +46,8 @@ app.get('/:log/:key', function(req, res){
     res.send("Wrong api key")
   }
 })
+
+//Add line to log
 app.get('/:log/:text/:key', function (req, res) {
   if(isValidApiKey(req.params.key)){
     res.set('Content-Type', 'text/plain')
@@ -50,6 +59,8 @@ app.get('/:log/:text/:key', function (req, res) {
     res.send("Wrong api key")
   }
 })
+
+//Start server
 http.listen(appPort, function(){
   console.log('listening on *:' + appPort)
 })
